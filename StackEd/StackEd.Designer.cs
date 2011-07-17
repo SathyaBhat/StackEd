b9f556d97d3941138662c8bf841ab8ea0e760d69
Sathya <sathya@sathyasays.com>
2011-07-17 13:16:09 +0530

    initial commit.

---
+++ StackEd/StackEd.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace StackEd
+{
+    partial class StackEd
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCorrect = new System.Windows.Forms.Button();
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.txtContent = new System.Windows.Forms.TextBox();
+            this.stsLogs = new System.Windows.Forms.StatusStrip();
+            this.stsLogLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.btnAutoCorrect = new System.Windows.Forms.Button();
+            this.stsLogs.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnCorrect
+            // 
+            this.btnCorrect.Location = new System.Drawing.Point(15, 12);
+            this.btnCorrect.Name = "btnCorrect";
+            this.btnCorrect.Size = new System.Drawing.Size(90, 26);
+            this.btnCorrect.TabIndex = 3;
+            this.btnCorrect.Text = "&Change Case";
+            this.btnCorrect.UseVisualStyleBackColor = true;
+            this.btnCorrect.Click += new System.EventHandler(this.btnCorrect_Click);
+            // 
+            // splitter1
+            // 
+            this.splitter1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.splitter1.Location = new System.Drawing.Point(0, 0);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(602, 49);
+            this.splitter1.TabIndex = 2;
+            this.splitter1.TabStop = false;
+            // 
+            // txtContent
+            // 
+            this.txtContent.AcceptsReturn = true;
+            this.txtContent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtContent.HideSelection = false;
+            this.txtContent.Location = new System.Drawing.Point(0, 49);
+            this.txtContent.Multiline = true;
+            this.txtContent.Name = "txtContent";
+            this.txtContent.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtContent.Size = new System.Drawing.Size(602, 397);
+            this.txtContent.TabIndex = 1;
+            this.txtContent.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtContent_KeyUp);
+            // 
+            // stsLogs
+            // 
+            this.stsLogs.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.stsLogLabel});
+            this.stsLogs.Location = new System.Drawing.Point(0, 424);
+            this.stsLogs.Name = "stsLogs";
+            this.stsLogs.Size = new System.Drawing.Size(602, 22);
+            this.stsLogs.TabIndex = 4;
+            this.stsLogs.Text = "statusStrip1";
+            // 
+            // stsLogLabel
+            // 
+            this.stsLogLabel.Name = "stsLogLabel";
+            this.stsLogLabel.Size = new System.Drawing.Size(0, 17);
+            // 
+            // btnAutoCorrect
+            // 
+            this.btnAutoCorrect.Location = new System.Drawing.Point(118, 12);
+            this.btnAutoCorrect.Name = "btnAutoCorrect";
+            this.btnAutoCorrect.Size = new System.Drawing.Size(77, 25);
+            this.btnAutoCorrect.TabIndex = 5;
+            this.btnAutoCorrect.Text = "Correct";
+            this.btnAutoCorrect.UseVisualStyleBackColor = true;
+            this.btnAutoCorrect.Click += new System.EventHandler(this.btnAutoCorrect_Click);
+            // 
+            // StackEd
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(602, 446);
+            this.Controls.Add(this.btnAutoCorrect);
+            this.Controls.Add(this.stsLogs);
+            this.Controls.Add(this.txtContent);
+            this.Controls.Add(this.btnCorrect);
+            this.Controls.Add(this.splitter1);
+            this.Name = "StackEd";
+            this.Text = "StackEd";
+            this.Load += new System.EventHandler(this.StackEd_Load);
+            this.stsLogs.ResumeLayout(false);
+            this.stsLogs.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCorrect;
+        private System.Windows.Forms.Splitter splitter1;
+        public System.Windows.Forms.TextBox txtContent;
+        private System.Windows.Forms.StatusStrip stsLogs;
+        private System.Windows.Forms.ToolStripStatusLabel stsLogLabel;
+        private System.Windows.Forms.Button btnAutoCorrect;
+    }
+}
+
---
+++ StackEd/StackEd.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace StackEd
+{
+    public partial class StackEd : Form
+    {
+        public string case_correction = "lower";
+
+        public StackEd()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void StackEd_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCorrect_Click(object sender, EventArgs e)
+        {
+             string content;
+         
+            if (txtContent.SelectedText.Equals(""))
+            {
+                content = txtContent.Text;
+               
+            }
+            else
+            {
+                content = txtContent.SelectedText;
+            }
+
+            stsLogLabel.Text = stsLogLabel.Text + ' ' + content;
+            switch (case_correction)
+            {
+                case "lower":
+                    content = convert_to_lower(content);
+                    case_correction = "title";
+                    break;
+                case "title":
+                    content = convert_to_titlecase(content);
+                    case_correction = "upper";
+                    break;
+                case "upper":
+                   content =  convert_to_upper(content);
+                    case_correction = "lower";
+                    break;
+                
+            }
+            stsLogLabel.Text = stsLogLabel.Text + ' ' + content;
+            stsLogLabel.Text = stsLogLabel.Text + ' ' + case_correction;
+
+            if (txtContent.SelectedText.Equals(""))
+            {
+                txtContent.Text = content;
+
+            }
+            else
+            {
+                txtContent.SelectedText = content;
+            }
+        }
+
+        private string convert_to_lower(string text_content)
+        {
+            string l_text;
+            l_text = text_content.ToLower(); 
+            return l_text;
+
+            
+        }
+
+        private string convert_to_upper(string text_content)
+        {
+            string u_text;
+            u_text = text_content.ToUpper();
+            return u_text;
+        }
+
+        private string convert_to_titlecase(string text_content)
+        {
+            string tc_text;
+            System.Globalization.CultureInfo cultureInfo =  System.Threading.Thread.CurrentThread.CurrentCulture;  
+            System.Globalization.TextInfo TextInfo = cultureInfo.TextInfo;
+            tc_text = TextInfo.ToTitleCase(text_content);
+            return tc_text;
+        }
+
+        private void auto_fix()
+        {
+            List<string> words = new List<string>(new string[] 
+            {
+          "AMD", "AppleScript", "ASUS", "ATI", "Bluetooth", "DivX", "DVD", "Eee PC", "FireWire",
+         "GarageBand", "GHz", "iBookstore", "iCal", "iChat", "iLife", "iMac", "iMovie", "iOS", "iPad",
+         "iPhone", "iPhoto", "iPod", "iTunes", "iWeb", "iWork", "JavaScript", "jQuery", "Lenovo", 
+         "MacBook", "MacPorts", "MHz", "Microsoft", "MobileMe", "MySQL", "Nvidia", "OS X", "PowerBook", "PowerPoint",
+         "QuickTime", "SSD", "TextEdit", "TextMate", "ThinkPad", "USB", "Vista", "VMware", "WebKit", "Wi-Fi",
+         "Windows XP", "Windows 7", "WordPress", "Xcode", "XMLHttpRequest", "Xserve"
+            }
+
+    );
+              
+            txtContent.Text = Regex.Replace(txtContent.Text, @"\bur\b", "your");
+            txtContent.Text = Regex.Replace(txtContent.Text, @"\bi( |')","I$1");
+            txtContent.Text = Regex.Replace(txtContent.Text, @"\bi ?m\b","I'm");
+            txtContent.Text = Regex.Replace(txtContent.Text, @"\bu\b","you");
+            txtContent.Text = Regex.Replace(txtContent.Text, @"\bur\b", "your");
+            txtContent.Text = Regex.Replace(txtContent.Text, @"\bcud\b", "could");
+            txtContent.Text = Regex.Replace(txtContent.Text, @"\bb4\b", "before", RegexOptions.IgnoreCase);
+            txtContent.Text = Regex.Replace(txtContent.Text, @"\bpl[sz]\b", "please", RegexOptions.IgnoreCase);
+            txtContent.Text = Regex.Replace(txtContent.Text, @"\bwin7\b", "Windows 7");
+            txtContent.Text = Regex.Replace(txtContent.Text, @"\bwinxp\b", "Windows XP");
+            txtContent.Text = Regex.Replace(txtContent.Text, @"\b(can|doesn|won|hasn|isn|didn)t\b", "$1't", RegexOptions.IgnoreCase);
+            txtContent.Text = Regex.Replace(txtContent.Text, @"\b(a)n(?= +(?![aeiou]|HTML|user))", "$1", RegexOptions.IgnoreCase);
+            txtContent.Text = Regex.Replace(txtContent.Text, @"\b(a)(?= +[aeiou](?!ser))", "$1n", RegexOptions.IgnoreCase);
+            txtContent.Text = Regex.Replace(txtContent.Text, @"\b(a)lot", "$1 lot", RegexOptions.IgnoreCase);
+            txtContent.Text = Regex.Replace(txtContent.Text, @"^(thx?|thanks?|cheers|thanx|tia)\s?((in advance)|you)?[\.\!\,]*", "", RegexOptions.IgnoreCase | RegexOptions.Multiline);
+            txtContent.Text = Regex.Replace(txtContent.Text, @"[ ]*\.( ?\.)+ *", ". ", RegexOptions.IgnoreCase | RegexOptions.Multiline);
+            txtContent.Text = Regex.Replace(txtContent.Text, @"[ ]*([\?\!] ?)+ *", "$1 ", RegexOptions.IgnoreCase | RegexOptions.Multiline);
+
+        }
+
+        private void btnAutoCorrect_Click(object sender, EventArgs e)
+        {
+            auto_fix();
+        }
+
+        private void txtContent_KeyUp(object sender, KeyEventArgs e)
+        {
+            if ( e.Control ) {
+            switch (e.KeyCode)
+            {
+                case ( Keys.A ):
+                    txtContent.SelectAll();
+                    break;
+            }
+            }
+        }
+    }
+}
